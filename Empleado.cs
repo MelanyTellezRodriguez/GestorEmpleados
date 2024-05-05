b2b713b8ef6d75a85a7e20ed0963d8220a4d57bd
melanytell1101@gmail.com <melanytell1101@gmail.com>
2024-05-04 19:14:05 -0600

    Se creo el proyecto GestorEmpleados

---
+++ Empleado.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorEmpleados
+{
+    internal class Empleado
+    {
+        //Variables miembro de la clase Empleado
+        private string Nombre;
+        private int NumeroEmpleado;
+        private decimal Salario;
+        private bool Activo; //Estado del empleado(activo o inactivo)
+
+        public Empleado(string nombre, int numeroEmpleado, decimal salario, bool activo = true)
+        {
+            this.Nombre = nombre;
+            this.NumeroEmpleado = numeroEmpleado;
+            this.Salario = salario;
+            this.Activo = activo;
+
+
+        }
+
+        //Metodo para mostrar los detalles del empleado
+        public void MostrarDetalles()
+        {
+            string estado = (Activo) ? "Activo" : "Inactivo";
+            Console.WriteLine($"Nombre: {Nombre}, Numero de empleado: {NumeroEmpleado}, Salario: {Salario:c}, Estado: {estado}");
+
+        }
+
+        //Metodo para obtener el numero de empleados
+        public int ObtenerNumeroEmpleado()
+        {
+            return this.NumeroEmpleado;
+        }
+
+        //Metodo para cambiar el estado del empleado
+        public void CambiarEstado(int nuevoEstado)
+        {
+            if (nuevoEstado == 1)
+            {
+                Activo = true;//Activar Empleado
+                Console.WriteLine("Empleado activo.");
+
+            }
+            else if (nuevoEstado == 0)
+            {
+                Activo = false;//Desactivar Empleado
+                Console.WriteLine("Empleado desactivado.");
+            }
+            else
+            {
+                Console.WriteLine("Estado no valido.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorEmpleados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplicacion de Gestion de Empleados");
+
+            //Crear una lista para almacenar empleados
+            List<Empleado> listaEmpleados = new List<Empleado>();
+
+
+            //Agregar empleados de ejemplos a la lista
+            listaEmpleados.Add(new Empleado("Juan", 101, 30000));
+            listaEmpleados.Add(new Empleado("Maria", 102, 35000));
+            listaEmpleados.Add(new Empleado("Pedro", 103, 32000, false));//Empleado inactivo
+
+            //Mostrar los detalles de cada empleado en la lista
+            foreach (var empleado in listaEmpleados)
+            {
+                empleado.MostrarDetalles();
+            }
+
+            //Cambiar el estado de un empleado por su numero de empleado
+
+            int numeroEmpleadoCambiarEstado = 103;//Numero de empleado a cambiar estado
+            Empleado empleadoCambiarEstado = BuscarEmpleado(listaEmpleados, numeroEmpleadoCambiarEstado);
+
+
+            if (empleadoCambiarEstado != null)
+            {
+                //Cambiar el estado a activo (1) o inactivo (0)
+                empleadoCambiarEstado.CambiarEstado(1);//Cambiar a activo
+                empleadoCambiarEstado.MostrarDetalles();//Mostrar detalles actualizados
+            }
+            else
+            {
+                Console.WriteLine($"Empleado con numero {numeroEmpleadoCambiarEstado} no encontrado.");
+            }
+
+
+
+        }
+
+        static Empleado BuscarEmpleado(List<Empleado> empleados, int numeroEmpleado)
+        {
+            foreach (var empleado in empleados)
+            {
+                if (empleado.ObtenerNumeroEmpleado() == numeroEmpleado)
+                {
+                    return empleado;
+                }
+            }
+            return null; // Retorna null si no se encuentra ningun empleado con ese numero
+
+        }
+    
+    }
+}
